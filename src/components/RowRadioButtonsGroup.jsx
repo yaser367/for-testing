ac2d081e2531271b0ce0fad4678d30d1e6eca183
yaser367 <yasermuhammed367@gmail.com>
2023-02-21 11:52:58 +0530

    added order now

--- src/components/RowRadioButtonsGroup.jsx
+++ src/components/RowRadioButtonsGroup.jsx
@@ -5,7 +5,7 @@ import FormControlLabel from "@mui/material/FormControlLabel";
 import FormControl from "@mui/material/FormControl";
 import FormLabel from "@mui/material/FormLabel";
 
-export default function RowRadioButtonsGroup({ header,game,setGame }) {
+export default function RowRadioButtonsGroup({ header,game,setGame,handleAvailable }) {
   return (
     <FormControl>
       <FormLabel id="demo-row-radio-buttons-group-label">{header}</FormLabel>
@@ -14,9 +14,9 @@ export default function RowRadioButtonsGroup({ header,game,setGame }) {
         aria-labelledby="demo-row-radio-buttons-group-label"
         name="row-radio-buttons-group"
       >
-        <FormControlLabel onChange={(e)=>setGame(e.target.value)} value="fives" control={<Radio />} label="5x5" />
-        <FormControlLabel onChange={(e)=>setGame(e.target.value)} value="sevens" control={<Radio />} label="7x7" />
-        <FormControlLabel onChange={(e)=>setGame(e.target.value)} value="elevens" control={<Radio />} label="11x11" />
+        <FormControlLabel onClick={handleAvailable} onChange={(e)=>setGame(e.target.value)} value="fives" control={<Radio />} label="5x5" />
+        <FormControlLabel onClick={handleAvailable} onChange={(e)=>setGame(e.target.value)} value="sevens" control={<Radio />} label="7x7" />
+        <FormControlLabel onClick={handleAvailable} onChange={(e)=>setGame(e.target.value)} value="elevens" control={<Radio />} label="11x11" />
         <FormControlLabel
           value="disabled"
           disabled
